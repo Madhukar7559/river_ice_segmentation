54eebf9d68c2c6e6518951e86bbb33f6faaec820
abhineet123 <asingh1@ualberta.ca>
2019-04-08 12:02:27 -0600

    minor fix

--- plotIceConcentration.py
+++ plotIceConcentration.py
@@ -16,6 +16,8 @@ parser.add_argument("--labels_ext", type=str, default='png')
 parser.add_argument("--seg_path", type=str, default='')
 parser.add_argument("--seg_ext", type=str, default='png')
 
+parser.add_argument("--out_path", type=str, default='')
+
 parser.add_argument("--out_ext", type=str, default='png')
 
 parser.add_argument("--save_path", type=str, default='')
@@ -40,6 +42,9 @@ images_path = args.images_path
 images_ext = args.images_ext
 labels_path = args.labels_path
 labels_ext = args.labels_ext
+
+out_path = args.out_path
+
 
 seg_path = args.seg_path
 seg_ext = args.seg_ext
@@ -76,6 +81,13 @@ if labels_path and seg_path and seg_ext:
 else:
     stitch = save_stitched = 1
 
+if not out_path:
+    out_path = labels_path+'_conc'
+    if not os.path.isdir(labels_path):
+        os.makedirs(out_path)
+    print('Writing concentration data to {}'.format(out_path))
+
+
 if not save_path:
     save_path = os.path.join(os.path.dirname(images_path), 'ice_concentration')
 
@@ -97,28 +109,9 @@ print('label_diff: {}'.format(label_diff))
 
 n_frames = end_id - start_id + 1
 
-pix_acc = np.zeros((n_frames,))
-
-mean_acc = np.zeros((n_frames,))
-# mean_acc_ice = np.zeros((n_frames,))
-# mean_acc_ice_1 = np.zeros((n_frames,))
-# mean_acc_ice_2 = np.zeros((n_frames,))
-#
-mean_IU = np.zeros((n_frames,))
-# mean_IU_ice = np.zeros((n_frames,))
-# mean_IU_ice_1 = np.zeros((n_frames,))
-# mean_IU_ice_2 = np.zeros((n_frames,))
-
-fw_IU = np.zeros((n_frames,))
-fw_sum = np.zeros((n_classes,))
 
 print_diff = int(n_frames * 0.01)
 
-avg_mean_acc_ice = avg_mean_acc_ice_1 = avg_mean_acc_ice_2 = 0
-avg_mean_IU_ice = avg_mean_IU_ice_1 = avg_mean_IU_ice_2 = 0
-
-skip_mean_acc_ice_1 = skip_mean_acc_ice_2 = 0
-skip_mean_IU_ice_1 = skip_mean_IU_ice_2 = 0
 _pause = 1
 labels_img = None
 
@@ -170,129 +163,21 @@ for img_id in range(start_id, end_id + 1):
         else:
             labels_img = np.copy(labels_img_orig)
 
-        if len(labels_img.shape) != 3:
-            labels_img = np.stack((labels_img, labels_img, labels_img), axis=2)
+        if len(labels_img.shape) == 3:
+            labels_img = labels_img[:, :, 0].squeeze()
+            # labels_img = np.stack((labels_img, labels_img, labels_img), axis=2)
 
-        if stitch:
-            stitched = np.concatenate((src_img, labels_img), axis=1)
+        conc_data_x = np.asarray(range(src_width), dtype=np.float64)
+        conc_data_y = np.zeros((src_width,), dtype=np.float64)
+        for i in range(src_width):
+            curr_pix = np.squeeze(labels_img[:, i])
+            ice_pix = curr_pix[curr_pix != 0]
 
-        if eval_mode:
-            seg_img_fname = os.path.join(seg_path, img_fname_no_ext + '.{}'.format(seg_ext))
-            seg_img = imread(seg_img_fname)
-            if seg_img is None:
-                raise SystemError('Segmentation image could not be read from: {}'.format(seg_img_fname))
+            conc_data_y[i] = len(ice_pix) / float(src_height)
 
-            if len(seg_img.shape) == 3:
-                seg_img = np.squeeze(seg_img[:, :, 0])
+        conc_data = np.concatenate([conc_data_x, conc_data_y], axis=1)
 
-            seg_height, seg_width = seg_img.shape
+        conc_data_fname = os.path.join(out_path, img_fname_no_ext + '.txt')
+        np.savetxt(conc_data_fname, conc_data, fmt='%.6f')
 
-            if seg_width == 2 * src_width or seg_width == 3 * src_width:
-                _start_id = seg_width - src_width
-                seg_img = seg_img[:, _start_id:]
 
-            # print('seg_img.shape: ', seg_img.shape)
-            # print('labels_img_orig.shape: ', labels_img_orig.shape)
-
-            pix_acc[img_id] = eval.pixel_accuracy(seg_img, labels_img_orig)
-            _acc, mean_acc[img_id] = eval.mean_accuracy(seg_img, labels_img_orig, return_acc=1)
-            _IU, mean_IU[img_id] = eval.mean_IU(seg_img, labels_img_orig, return_iu=1)
-            fw_IU[img_id], _fw = eval.frequency_weighted_IU(seg_img, labels_img_orig, return_freq=1)
-
-            fw_sum += _fw
-            _fw_total = np.sum(_fw)
-
-            # print('_fw: {}'.format(_fw))
-            # print('_fw_total: {}'.format(_fw_total))
-
-            _fw_frac = np.array(_fw) / float(_fw_total)
-
-            print('_fw_frac: {}'.format(_fw_frac))
-
-            mean_acc_ice = np.mean(list(_acc.values())[1:])
-            avg_mean_acc_ice += (mean_acc_ice - avg_mean_acc_ice) / (img_id + 1)
-            try:
-                mean_acc_ice_1 = _acc[1]
-                avg_mean_acc_ice_1 += (mean_acc_ice_1 - avg_mean_acc_ice_1) / (img_id - skip_mean_acc_ice_1 + 1)
-            except KeyError:
-                print('\nskip_mean_acc_ice_1: {}'.format(img_id))
-                skip_mean_acc_ice_1 += 1
-            try:
-                mean_acc_ice_2 = _acc[2]
-                avg_mean_acc_ice_2 += (mean_acc_ice_2 - avg_mean_acc_ice_2) / (img_id - skip_mean_acc_ice_2 + 1)
-            except KeyError:
-                print('\nskip_mean_acc_ice_2: {}'.format(img_id))
-                skip_mean_acc_ice_2 += 1
-
-            mean_IU_ice = np.mean(list(_IU.values())[1:])
-            avg_mean_IU_ice += (mean_IU_ice - avg_mean_IU_ice) / (img_id + 1)
-            try:
-                mean_IU_ice_1 = _IU[1]
-                avg_mean_IU_ice_1 += (mean_IU_ice_1 - avg_mean_IU_ice_1) / (img_id - skip_mean_IU_ice_1 + 1)
-            except KeyError:
-                print('\nskip_mean_IU_ice_1: {}'.format(img_id))
-                skip_mean_IU_ice_1 += 1
-            try:
-                mean_IU_ice_2 = _IU[2]
-                avg_mean_IU_ice_2 += (mean_IU_ice_2 - avg_mean_IU_ice_2) / (img_id - skip_mean_IU_ice_2 + 1)
-            except KeyError:
-                print('\nskip_mean_IU_ice_2: {}'.format(img_id))
-                skip_mean_IU_ice_2 += 1
-
-            seg_img = (seg_img * label_diff).astype(np.uint8)
-            if len(seg_img.shape) != 3:
-                seg_img = np.stack((seg_img, seg_img, seg_img), axis=2)
-
-            if stitch and stitch_seg:
-                stitched = np.concatenate((stitched, seg_img), axis=1)
-            if not stitch and show_img:
-                cv2.imshow('seg_img', seg_img)
-
-    if stitch:
-        if save_stitched:
-            seg_save_path = os.path.join(save_path, '{}.{}'.format(img_fname_no_ext, out_ext))
-            imsave(seg_save_path, stitched)
-        if show_img:
-            cv2.imshow('stitched', stitched)
-    else:
-        if show_img:
-            cv2.imshow('src_img', src_img)
-            if labels_path:
-                cv2.imshow('labels_img', labels_img)
-
-    if show_img:
-        k = cv2.waitKey(1 - _pause)
-        if k == 27:
-            sys.exit(0)
-        elif k == 32:
-            _pause = 1 - _pause
-    img_done = img_id - start_id + 1
-    if img_done % print_diff == 0:
-        log_txt = 'Done {:5d}/{:5d} frames'.format(img_done, n_frames)
-        if eval_mode:
-            log_txt = '{:s} pix_acc: {:.5f} mean_acc: {:.5f} mean_IU: {:.5f} fw_IU: {:.5f}' \
-                      ' acc_ice: {:.5f} acc_ice_1: {:.5f} acc_ice_2: {:.5f}' \
-                      ' IU_ice: {:.5f} IU_ice_1: {:.5f} IU_ice_2: {:.5f}'.format(
-                log_txt, pix_acc[img_id], mean_acc[img_id], mean_IU[img_id], fw_IU[img_id],
-                avg_mean_acc_ice, avg_mean_acc_ice_1, avg_mean_acc_ice_2,
-                avg_mean_IU_ice, avg_mean_IU_ice_1, avg_mean_IU_ice_2,
-            )
-        print_and_write(log_txt, log_fname)
-
-sys.stdout.write('\n')
-sys.stdout.flush()
-
-if eval_mode:
-    log_txt = "pix_acc\t mean_acc\t mean_IU\t fw_IU\n{:.5f}\t{:.5f}\t{:.5f}\t{:.5f}\n".format(
-        np.mean(pix_acc), np.mean(mean_acc), np.mean(mean_IU), np.mean(fw_IU))
-    log_txt += "mean_acc_ice\t mean_acc_ice_1\t mean_acc_ice_2\n{:.5f}\t{:.5f}\t{:.5f}\n".format(
-        avg_mean_acc_ice, avg_mean_acc_ice_1, avg_mean_acc_ice_2)
-    log_txt += "mean_IU_ice\t mean_IU_ice_1\t mean_IU_ice_2\n{:.5f}\t{:.5f}\t{:.5f}\n".format(
-        avg_mean_IU_ice, avg_mean_IU_ice_1, avg_mean_IU_ice_2)
-    print_and_write(log_txt, log_fname)
-
-    fw_sum_total = np.sum(fw_sum)
-    fw_sum_frac = fw_sum / float(fw_sum_total)
-
-    print('fw_sum_total: {}'.format(fw_sum_total))
-    print('fw_sum_frac: {}'.format(fw_sum_frac))
