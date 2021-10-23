739dfc5de60c79ca1ba1827db20db806e2b08e2b
abhineet123 <asingh1@ualberta.ca>
2021-10-23 02:45:03 +0100

    minor fix

--- new_deeplab/datasets/build_ipsc_data.py
+++ new_deeplab/datasets/build_ipsc_data.py
@@ -1,19 +1,4 @@
-# Copyright 2018 The TensorFlow Authors All Rights Reserved.
-#
-# Licensed under the Apache License, Version 2.0 (the "License");
-# you may not use this file except in compliance with the License.
-# You may obtain a copy of the License at
-#
-#   http://www.apache.org/licenses/LICENSE-2.0
-#
-# Unless required by applicable law or agreed to in writing, software
-# distributed under the License is distributed on an "AS IS" BASIS,
-# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-# See the License for the specific language governing permissions and
-# limitations under the License.
-# ==============================================================================
-
-"""Converts CTC data to TFRecord file format with Example protos."""
+"""Converts IPSC data to TFRecord file format with Example protos."""
 
 import math
 import os
@@ -76,128 +61,11 @@ class Params:
         self.db_splits = IPSCInfo.DBSplits().__dict__
 
 
-def seg_to_png(gold_seg_src_file_ids, silver_seg_src_file_ids, img_src_file_id,
-               silver_seg_path, gold_seg_path, png_seg_src_path, img_src_file, two_classes):
-    gold_seg_img = silver_seg_img = None
-
-    try:
-        gold_seg_src_file = gold_seg_src_file_ids[img_src_file_id]
-    except KeyError:
-        n_gold_seg_objs = 0
-    else:
-        gold_seg_src_path = os.path.join(gold_seg_path, gold_seg_src_file)
-        gold_seg_img = cv2.imread(gold_seg_src_path, cv2.IMREAD_UNCHANGED)
-        gold_seg_obj_ids = list(np.unique(gold_seg_img, return_counts=False))
-        gold_seg_obj_ids.remove(0)
-
-        n_gold_seg_objs = len(gold_seg_obj_ids)
-
-    try:
-        silver_seg_src_file = silver_seg_src_file_ids[img_src_file_id]
-    except KeyError:
-        n_silver_seg_objs = 0
-    else:
-        silver_seg_src_path = os.path.join(silver_seg_path, silver_seg_src_file)
-        silver_seg_img = cv2.imread(silver_seg_src_path, cv2.IMREAD_UNCHANGED)
-
-        silver_seg_obj_ids = list(np.unique(silver_seg_img, return_counts=False))
-        silver_seg_obj_ids.remove(0)
-
-        n_silver_seg_objs = len(silver_seg_obj_ids)
-
-    if n_silver_seg_objs == 0 and n_gold_seg_objs == 0:
-        # print('\nno segmentations found for {}\n'.format(img_src_file))
-        return 0
-
-    if n_silver_seg_objs > n_gold_seg_objs:
-        seg_img = silver_seg_img
-    else:
-        seg_img = gold_seg_img
-
-    if two_classes:
-        seg_img[seg_img > 0] = 1
-        seg_img = seg_img.astype(np.uint8)
-
-    cv2.imwrite(png_seg_src_path, seg_img)
-
-    return 1
-
-
 def linux_path(*args, **kwargs):
     return os.path.join(*args, **kwargs).replace(os.sep, '/')
 
 
-def _convert_dataset(params):
-    """
-
-    :param Params params:
-    :return:
-    """
-
-    seq_ids = params.db_splits[params.db_split]
-
-    print('root_dir: {}'.format(params.root_dir))
-    print('db_split: {}'.format(params.db_split))
-    print('seq_ids: {}'.format(seq_ids))
-    print('output_dir: {}'.format(params.output_dir))
-
-    jpg_img_root_path = linux_path(params.root_dir, 'CTC', 'Images')
-    os.makedirs(params.output_dir, exist_ok=True)
-
-    if params.disable_seg:
-        print('\nSegmentations are disabled\n')
-
-    if params.start_id > 0:
-        seq_ids = seq_ids[params.start_id:]
-
-    n_seq = len(seq_ids)
-    img_root_path = jpg_img_root_path
-    img_exts = ('.jpg',)
-
-    if not params.disable_seg:
-        seg_src_file_ids = {}
-
-    img_src_file_ids = {}
-
-    n_total_src_files = 0
-
-    img_src_files = []
-    for __id, seq_id in enumerate(seq_ids):
-
-        seq_name, n_frames = IPSCInfo.sequences[seq_id]
-
-        print('\tseq {} / {}\t{}\t{}\t{} frames'.format(__id + 1, n_seq, seq_id, seq_name, n_frames))
-
-        img_dir_path = linux_path(params.root_dir, seq_name, 'images')
-
-        _img_src_files = [linux_path(img_dir_path, k) for k in os.listdir(img_dir_path) if
-                          os.path.splitext(k.lower())[1] in img_exts]
-        _img_src_files.sort()
-
-        n_img_src_files = len(_img_src_files)
-
-        n_total_src_files += len(_img_src_files)
-
-        assert n_img_src_files == n_frames, \
-            "Mismatch between number of the specified frames and number of actual images in folder"
-
-        if not params.disable_seg:
-            seg_path = linux_path(params.root_dir, seq_name, 'labels')
-
-            assert os.path.exists(seg_path), \
-                "segmentations found for sequence: {}".format(seq_name)
-
-            seg_src_files = [linux_path(seg_path, k) for k in os.listdir(seg_path) if
-                             os.path.splitext(k.lower())[1] in ('.png',)]
-            seg_src_files.sort()
-            n_seg_src_files = len(seg_src_files)
-
-            assert n_img_src_files == n_seg_src_files, "mismatch between number of source and segmentation images"
-
-    create_tfrecords(img_src_files, img_src_file_ids, params.num_shards, params.db_split, params.output_dir)
-
-
-def create_tfrecords(img_src_files, seg_src_files, n_shards, sub_seq, output_dir):
+def create_tfrecords(img_src_files, seg_src_files, n_shards, db_split, output_dir):
     image_reader = build_data.ImageReader('jpeg', channels=1)
 
     label_reader = build_data.ImageReader('png', channels=1)
@@ -213,7 +81,7 @@ def create_tfrecords(img_src_files, seg_src_files, n_shards, sub_seq, output_dir
 
         output_file_path = os.path.join(
             output_dir,
-            '{:s}-{:05d}-of-{:05d}.tfrecord'.format(sub_seq, shard_id, n_shards))
+            '{:s}-{:05d}-of-{:05d}.tfrecord'.format(db_split, shard_id, n_shards))
 
         with tf.python_io.TFRecordWriter(output_file_path) as tfrecord_writer:
             start_idx = shard_id * n_per_shard
@@ -239,6 +107,73 @@ def create_tfrecords(img_src_files, seg_src_files, n_shards, sub_seq, output_dir
                     image_data, img_src_path, height, width, seg_data)
 
                 tfrecord_writer.write(example.SerializeToString())
+
+
+def _convert_dataset(params):
+    """
+
+    :param Params params:
+    :return:
+    """
+
+    seq_ids = params.db_splits[params.db_split]
+
+    print('root_dir: {}'.format(params.root_dir))
+    print('db_split: {}'.format(params.db_split))
+    print('seq_ids: {}'.format(seq_ids))
+    print('output_dir: {}'.format(params.output_dir))
+
+    os.makedirs(params.output_dir, exist_ok=True)
+
+    if params.disable_seg:
+        print('\nSegmentations are disabled\n')
+
+    if params.start_id > 0:
+        seq_ids = seq_ids[params.start_id:]
+
+    n_seq = len(seq_ids)
+    img_exts = ('.jpg',)
+
+    n_total_src_files = 0
+
+    img_src_files = []
+    seg_src_files = []
+    for __id, seq_id in enumerate(seq_ids):
+
+        seq_name, n_frames = IPSCInfo.sequences[seq_id]
+
+        print('\tseq {} / {}\t{}\t{}\t{} frames'.format(__id + 1, n_seq, seq_id, seq_name, n_frames))
+
+        img_dir_path = linux_path(params.root_dir, seq_name, 'images')
+
+        _img_src_files = [linux_path(img_dir_path, k) for k in os.listdir(img_dir_path) if
+                          os.path.splitext(k.lower())[1] in img_exts]
+        _img_src_files.sort()
+
+        img_src_files += _img_src_files
+
+        n_img_src_files = len(_img_src_files)
+
+        n_total_src_files += len(_img_src_files)
+
+        assert n_img_src_files == n_frames, \
+            "Mismatch between number of the specified frames and number of actual images in folder"
+
+        if not params.disable_seg:
+            seg_path = linux_path(params.root_dir, seq_name, 'labels')
+
+            assert os.path.exists(seg_path), \
+                "segmentations found for sequence: {}".format(seq_name)
+
+            _seg_src_files = [linux_path(seg_path, k) for k in os.listdir(seg_path) if
+                              os.path.splitext(k.lower())[1] in ('.png',)]
+            _seg_src_files.sort()
+            n_seg_src_files = len(_seg_src_files)
+
+            assert n_img_src_files == n_seg_src_files, "mismatch between number of source and segmentation images"
+            seg_src_files += _seg_src_files
+
+    create_tfrecords(img_src_files, seg_src_files, params.num_shards, params.db_split, params.output_dir)
 
 
 def main():
